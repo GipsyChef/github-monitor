f16460d5ca3ac99240ccf069f3cfc4793b9c5767
Yoni <cigan1@gmail.com>
2026-05-11 13:50:21 -0400

    Notificaitions enabled

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  const url = event.notification.data?.url || "/";
+  event.waitUntil(
+    self.clients.matchAll({ type: "window", includeUncontrolled: true }).then((clients) => {
+      for (const client of clients) {
+        if ("focus" in client) return client.focus();
+      }
+      if (self.clients.openWindow) return self.clients.openWindow(url);
+      return undefined;
+    })
+  );
+});
